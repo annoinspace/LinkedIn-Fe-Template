5c640e16895ed8fee012ab3aed6a4f3716aeb328
annoinspace <aneesahalmask@gmail.com>
2023-01-24 22:04:02 +0000

    navbar fixed

--- src/components/global/CustomNavbar.jsx
+++ src/components/global/CustomNavbar.jsx
@@ -1,221 +1,211 @@
-import { useState } from "react";
-import { Container, Navbar, Nav, ListGroup, Button } from "react-bootstrap";
-import * as Icon from "react-bootstrap-icons";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import SearchField from "./SearchField";
+import { useState } from "react"
+import { Container, Navbar, Nav, ListGroup, Button } from "react-bootstrap"
+import * as Icon from "react-bootstrap-icons"
+import { useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+import SearchField from "./SearchField"
 
 const CustomNavbar = () => {
-  const navigate = useNavigate();
-  const [meSelected, setMeSelected] = useState(false);
-  const user = useSelector((state) => state.user.user);
-  const userName = useSelector((state) => state.myProfile.detailsData.name);
-  const userTitle = useSelector((state) => state.myProfile.detailsData.title);
-  const userImage = useSelector((state) => state.myProfile.detailsData.image);
-  const userSurname = useSelector(
-    (state) => state.myProfile.detailsData.surname
-  );
+  const navigate = useNavigate()
+  const [meSelected, setMeSelected] = useState(false)
+  const user = useSelector((state) => state.user.user)
+  const userName = useSelector((state) => state.myProfile.detailsData.name)
+  const userTitle = useSelector((state) => state.myProfile.detailsData.title)
+  const userImage = useSelector((state) => state.myProfile.detailsData.image)
+  const userSurname = useSelector((state) => state.myProfile.detailsData.surname)
 
   return (
     <>
-      <Navbar expand="lg" variant="light" bg="white" className="sticky-top">
-        <Container className="d-flex justify-content-between align-items-center navbar-font-sizes">
-          <div
-            onClick={() => {
-              setMeSelected(false);
-            }}
-            className="d-flex"
-          >
-            <div className="my-auto">
-              <Navbar.Brand href="/home">
-                <Icon.Linkedin className="navbar-logo-icon" />
-              </Navbar.Brand>
-            </div>
-            <SearchField />
-          </div>
-
-          <Nav className="rightSideNavbarContainer d-flex align-items-center justify-content-center flex-row mx-auto mx-md-none">
-            <Link
+      {user && (
+        <Navbar expand="lg" variant="light" bg="white" className="sticky-top">
+          <Container className="d-flex justify-content-between align-items-center navbar-font-sizes">
+            <div
               onClick={() => {
-                setMeSelected(false);
+                setMeSelected(false)
               }}
-              to={"/"}
+              className="d-flex"
             >
-              <div className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none">
+              <div className="my-auto">
+                <Navbar.Brand href="/home">
+                  <Icon.Linkedin className="navbar-logo-icon" />
+                </Navbar.Brand>
+              </div>
+              <SearchField />
+            </div>
+
+            <Nav className="rightSideNavbarContainer d-flex align-items-center justify-content-center flex-row mx-auto mx-md-none">
+              <Link
+                onClick={() => {
+                  setMeSelected(false)
+                }}
+                to={"/"}
+              >
+                <div className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none">
+                  <div>
+                    <Icon.HouseDoorFill className="mr-sm-3 mr-md-0" />
+                  </div>
+                  <div className="d-none d-md-block">Home</div>
+                </div>
+              </Link>
+
+              <div
+                onClick={() => {
+                  setMeSelected(false)
+                }}
+                className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none"
+              >
                 <div>
-                  <Icon.HouseDoorFill className="mr-sm-3 mr-md-0" />
+                  <Icon.PeopleFill className="mr-sm-3 mr-md-0" />
                 </div>
-                <div className="d-none d-md-block">Home</div>
+                <div className="d-none d-md-block">My Network</div>
               </div>
-            </Link>
-
-            <div
-              onClick={() => {
-                setMeSelected(false);
-              }}
-              className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none"
-            >
-              <div>
-                <Icon.PeopleFill className="mr-sm-3 mr-md-0" />
+              <div
+                onClick={() => {
+                  setMeSelected(false)
+                }}
+                className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none"
+              >
+                <div>
+                  <Icon.BriefcaseFill className="mr-sm-3 mr-md-0" />
+                </div>
+                <div className="d-none d-md-block">Jobs</div>
               </div>
-              <div className="d-none d-md-block">My Network</div>
-            </div>
-            <div
-              onClick={() => {
-                setMeSelected(false);
-              }}
-              className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none"
-            >
-              <div>
-                <Icon.BriefcaseFill className="mr-sm-3 mr-md-0" />
+              <div
+                onClick={() => {
+                  setMeSelected(false)
+                }}
+                className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none"
+              >
+                <div>
+                  <Icon.ChatRightDotsFill className="mr-sm-3 mr-md-0" />
+                </div>
+                <div className="d-none d-md-block">Messaging</div>
               </div>
-              <div className="d-none d-md-block">Jobs</div>
-            </div>
-            <div
-              onClick={() => {
-                setMeSelected(false);
-              }}
-              className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none"
-            >
-              <div>
-                <Icon.ChatRightDotsFill className="mr-sm-3 mr-md-0" />
+              <div
+                onClick={() => {
+                  setMeSelected(false)
+                }}
+                className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none"
+              >
+                <div>
+                  <Icon.BellFill className="mr-sm-3 mr-md-0" />
+                </div>
+                <div className="d-none d-md-block">Notifications</div>
               </div>
-              <div className="d-none d-md-block">Messaging</div>
-            </div>
-            <div
-              onClick={() => {
-                setMeSelected(false);
-              }}
-              className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none"
-            >
-              <div>
-                <Icon.BellFill className="mr-sm-3 mr-md-0" />
-              </div>
-              <div className="d-none d-md-block">Notifications</div>
-            </div>
-            <div
-              className=" test nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none position-relative"
-              onClick={() => {
-                if (meSelected === false) {
-                  setMeSelected(true);
-                } else {
-                  setMeSelected(false);
-                }
-              }}
-            >
-              <div className="me-icon-container d-sm-flex my-auto d-md-block my-md-0">
-                <img
-                  src={user[0].pfp}
-                  alt=""
-                  className="d-flex align-items-center justify-content-center"
-                />
-              </div>
-              <div className="d-flex justify-content-center align-items-center">
-                <div className="d-none d-md-block">Me</div>
-                <Icon.CaretDownFill className="d-none d-md-block" />
-                <div
-                  className={
-                    meSelected === false
-                      ? "d-flex justify-content-between me-dropdown-list invisible"
-                      : "d-flex justify-content-between me-dropdown-list visible"
+              <div
+                className=" test nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover mr-4 mr-sm-2 mr-md-none position-relative"
+                onClick={() => {
+                  if (meSelected === false) {
+                    setMeSelected(true)
+                  } else {
+                    setMeSelected(false)
                   }
-                >
-                  <ListGroup style={{ width: "350px" }}>
-                    <ListGroup.Item>
-                      <div className="d-flex mb-2">
-                        <div className="me-dropdown-image">
-                          <img
-                            src={user[0].pfp}
-                            alt=""
-                            className="d-flex align-items-center justify-content-center"
-                          />
-                        </div>
-                        <div className="ml-2 underline-on-hover">
-                          <div className="me-dropdown-big-element">
-                            {user[0].name} {user[0].surname}
+                }}
+              >
+                <div className="me-icon-container d-sm-flex my-auto d-md-block my-md-0">
+                  {user.pfp && (
+                    <img src={user.pfp} alt="" className="d-flex align-items-center justify-content-center" />
+                  )}
+                </div>
+                <div className="d-flex justify-content-center align-items-center">
+                  <div className="d-none d-md-block">Me</div>
+                  <Icon.CaretDownFill className="d-none d-md-block" />
+                  <div
+                    className={
+                      meSelected === false
+                        ? "d-flex justify-content-between me-dropdown-list invisible"
+                        : "d-flex justify-content-between me-dropdown-list visible"
+                    }
+                  >
+                    <ListGroup style={{ width: "350px" }}>
+                      <ListGroup.Item>
+                        <div className="d-flex mb-2">
+                          {user.pfp && (
+                            <div className="me-dropdown-image">
+                              <img src={user.pfp} alt="" className="d-flex align-items-center justify-content-center" />
+                            </div>
+                          )}
+                          <div className="ml-2 underline-on-hover">
+                            <div className="me-dropdown-big-element">
+                              {user.name} {user.surname}
+                            </div>
+                            <div>{user.job}</div>
+                            <div>{user.Title}</div>
                           </div>
-                          <div>{user[0].job}</div>
-                          <div>{userTitle}</div>
                         </div>
-                      </div>
-                      <div className="d-flex justify-content-center">
-                        <Button
-                          className="me-dropdown-button"
-                          onClick={() => {
-                            navigate("/me");
-                            setMeSelected(false);
-                          }}
-                        >
-                          View Profile
-                        </Button>
-                      </div>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="underline-on-hover">
-                      <div className="me-dropdown-big-element">Account</div>
-                      <div>Settings & Privacy</div>
-                      <div>Help</div>
-                      <div>Language</div>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="underline-on-hover">
-                      <div className="me-dropdown-big-element">Manage</div>
-                      <div>Posts & Activity</div>
-                      <div>Job Posting Account</div>
-                    </ListGroup.Item>
-                    <ListGroup.Item className="underline-on-hover">
-                      <div>Sing Out</div>
-                    </ListGroup.Item>
-                  </ListGroup>
+                        <div className="d-flex justify-content-center">
+                          <Button
+                            className="me-dropdown-button"
+                            onClick={() => {
+                              navigate("/me")
+                              setMeSelected(false)
+                            }}
+                          >
+                            View Profile
+                          </Button>
+                        </div>
+                      </ListGroup.Item>
+                      <ListGroup.Item className="underline-on-hover">
+                        <div className="me-dropdown-big-element">Account</div>
+                        <div>Settings & Privacy</div>
+                        <div>Help</div>
+                        <div>Language</div>
+                      </ListGroup.Item>
+                      <ListGroup.Item className="underline-on-hover">
+                        <div className="me-dropdown-big-element">Manage</div>
+                        <div>Posts & Activity</div>
+                        <div>Job Posting Account</div>
+                      </ListGroup.Item>
+                      <ListGroup.Item className="underline-on-hover">
+                        <div>Sing Out</div>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div
-              onClick={() => {
-                setMeSelected(false);
-              }}
-              className="nav-link d-flex flex-column justify-content-around align-items-center border-left cursor-on-hover ml-0 ml-sm-2 ml-md-none pl-4 pl-sm-2 pl-md-none"
-            >
-              <div className="d-sm-lex my-auto d-md-block my-md-0">
-                <Icon.Grid3x3GapFill className="mr-sm-3 mr-md-0" />
-              </div>
-              <div className="d-flex justify-content-center align-items-center">
-                <div className="d-none d-md-block">Work</div>
-                <Icon.CaretDownFill className="d-none d-md-block" />
-              </div>
-            </div>
-            <div
-              onClick={() => {
-                setMeSelected(false);
-              }}
-              className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover ml-4 ml-sm-2 ml-md-none"
-            >
               <div
                 onClick={() => {
-                  setMeSelected(false);
+                  setMeSelected(false)
                 }}
+                className="nav-link d-flex flex-column justify-content-around align-items-center border-left cursor-on-hover ml-0 ml-sm-2 ml-md-none pl-4 pl-sm-2 pl-md-none"
               >
-                <Icon.CardChecklist />
+                <div className="d-sm-lex my-auto d-md-block my-md-0">
+                  <Icon.Grid3x3GapFill className="mr-sm-3 mr-md-0" />
+                </div>
+                <div className="d-flex justify-content-center align-items-center">
+                  <div className="d-none d-md-block">Work</div>
+                  <Icon.CaretDownFill className="d-none d-md-block" />
+                </div>
               </div>
               <div
                 onClick={() => {
-                  setMeSelected(false);
+                  setMeSelected(false)
                 }}
-                className="d-none d-md-block"
+                className="nav-link d-flex flex-column justify-content-around align-items-center cursor-on-hover ml-4 ml-sm-2 ml-md-none"
               >
-                Learning
+                <div
+                  onClick={() => {
+                    setMeSelected(false)
+                  }}
+                >
+                  <Icon.CardChecklist />
+                </div>
+                <div
+                  onClick={() => {
+                    setMeSelected(false)
+                  }}
+                  className="d-none d-md-block"
+                >
+                  Learning
+                </div>
               </div>
-            </div>
-          </Nav>
-        </Container>
-      </Navbar>
-      {/* <div
-        onClick={() => {
-          setMeSelected(false);
-        }}
-        className="containerTest"
-      ></div> */}
+            </Nav>
+          </Container>
+        </Navbar>
+      )}
     </>
-  );
-};
+  )
+}
 
-export default CustomNavbar;
+export default CustomNavbar
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,25 +1,25 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import displayUserSearchReducer from "../reducers/displayUserSearchReducer";
-import expandMesengerReducer from "../reducers/expandMessengerReducer";
-import getUserReducer from "../reducers/getUserReducer";
-import getUsersFromSearchReducer from "../reducers/getUsersFromSearchReducer";
-import experiencesReducer from "../reducers/experiencesReducer";
-import myProfileReducer from "../reducers/myProfileReducer";
-import setOtherUserReducer from "../reducers/setOtherUserReducer";
-import addPostModalReducer from "../reducers/addPostModalReducer";
-import getFeedPostsReducer from "../reducers/getFeedPostsReducer";
-import selectedFeedPostReducer from "../reducers/selectedFeedPostReducer";
-import editFeedPostModalReducer from "../reducers/editFeedPostModalReducer";
-import localStorage from "redux-persist/lib/storage";
-import addMainLogin from "../reducers/LoginReducer";
-import { persistReducer, persistStore } from "redux-persist";
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+import displayUserSearchReducer from "../reducers/displayUserSearchReducer"
+import expandMesengerReducer from "../reducers/expandMessengerReducer"
+import getUserReducer from "../reducers/getUserReducer"
+import getUsersFromSearchReducer from "../reducers/getUsersFromSearchReducer"
+import experiencesReducer from "../reducers/experiencesReducer"
+import myProfileReducer from "../reducers/myProfileReducer"
+import setOtherUserReducer from "../reducers/setOtherUserReducer"
+import addPostModalReducer from "../reducers/addPostModalReducer"
+import getFeedPostsReducer from "../reducers/getFeedPostsReducer"
+import selectedFeedPostReducer from "../reducers/selectedFeedPostReducer"
+import editFeedPostModalReducer from "../reducers/editFeedPostModalReducer"
+import localStorage from "redux-persist/lib/storage"
+import addMainLogin from "../reducers/LoginReducer"
+import { persistReducer, persistStore } from "redux-persist"
 
 // configureStore will set up the Redux Store for us!
 
 const persistConfig = {
   key: "root",
-  storage: localStorage,
-};
+  storage: localStorage
+}
 
 const bigReducer = combineReducers({
   // cart: cartReducer,
@@ -34,16 +34,20 @@ const bigReducer = combineReducers({
   feedPosts: getFeedPostsReducer,
   editThisPost: selectedFeedPostReducer,
   editPostModal: editFeedPostModalReducer,
-  user: addMainLogin,
-});
+  user: addMainLogin
+})
 
-const persistedReducer = persistReducer(persistConfig, bigReducer);
+const persistedReducer = persistReducer(persistConfig, bigReducer)
 
 export const store = configureStore({
   reducer: persistedReducer, // here there's place for just 1 value!
-});
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false
+    })
+})
 
-export const persistor = persistStore(store);
+export const persistor = persistStore(store)
 
 // now the store is ready! let's INJECT IT into our REACT APP!
 // we do it in the src/index.js file
