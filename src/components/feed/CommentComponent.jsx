13984fc26174d699d6f314e21136bd3d29776220
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2023-01-25 11:55:59 +0000

    Can delete comments only belonging to logged in user

--- src/components/feed/CommentComponent.jsx
+++ src/components/feed/CommentComponent.jsx
@@ -1,31 +1,46 @@
 import { Container, ListGroup, Row, Col, Collapse } from "react-bootstrap";
-import { useState } from "react";
+import { useSelector } from "react-redux";
+import { TiDeleteOutline } from "react-icons/ti";
+import { deleteCommentAction } from "../../redux/actions";
+import { useDispatch } from "react-redux";
 
 const CommentComp = ({ comment }) => {
+  const currentUserId = useSelector((state) => state.myProfile.detailsData._id);
+  const dispatch = useDispatch();
 
   return (
-        <ListGroup.Item
-          style={{ backgroundColor: "#F2F2F2", borderRadius: "20px" }}
-          className="m-3"
-        >
-          <div className="d-flex flex-row">
-            <div className="border recommended-user-image">
-              <img src={comment.author[0]?.pfp} alt="" />
-            </div>
-            <div className="d-flex flex-column">
-              <div className="mb-0 small-header-text bolder feed-text-name">
-                {comment.author[0].name} {comment.author[0].surname}
-              </div>
-              <div
-                style={{ fontSize: "0.6rem" }}
-                className="mt-1 text-muted feed-text-name"
+    <ListGroup.Item
+      style={{ backgroundColor: "#F2F2F2", borderRadius: "20px" }}
+      className="m-1"
+    >
+      <div className="d-flex flex-row">
+        <div className="border recommended-user-image">
+          <img src={comment.author[0]?.pfp} alt="" />
+        </div>
+        <div className="d-flex flex-column">
+          <div className="mb-0 small-header-text bolder feed-text-name">
+            {comment.author[0].name} {comment.author[0].surname}{" "}
+            {currentUserId === comment.author[0]._id ? (
+              <button
+                style={{ background: "none", border: "none", color: "red" }}
+                onClick={() => dispatch(deleteCommentAction(comment._id))}
               >
-                {comment.author[0].job}
-              </div>
-              <Container className="p-0 m-0">{comment.text}</Container>
-            </div>
+                <TiDeleteOutline />
+              </button>
+            ) : (
+              ""
+            )}
           </div>
-        </ListGroup.Item>
+          <div
+            style={{ fontSize: "0.6rem" }}
+            className="mt-1 text-muted feed-text-name"
+          >
+            {comment.author[0].job}
+          </div>
+          <Container className="p-0 m-0">{comment.text}</Container>
+        </div>
+      </div>
+    </ListGroup.Item>
   );
 };
 
