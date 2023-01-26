a5053b886ac86926a80891de39e31e0afeae9d22
annoinspace <aneesahalmask@gmail.com>
2023-01-26 11:13:37 +0000

    started connections

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import ExperienceComponent from "./components/center/ExperienceComponent";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import ResourcesSinglePageComponent from "./components/center/ResourcesSinglePageComponent.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MyProfile from "./components/global/MyProfile";
-import CustomNavbar from "./components/global/CustomNavbar.jsx";
-import { useSelector, useDispatch } from "react-redux";
-import { showUserSearchAction } from "./redux/actions";
-import MessagingPopup from "./components/global/MessagingPopup";
-import Feed from "./components/feed/Feed";
-import SearchDisplay from "./components/global/SearchDisplay.jsx";
-import OtherUser from "./components/global/OtherUser";
-import LoginPage from "./components/login/LoginPage";
-import Network from "./components/network/Network";
-import SignUp from "./components/sign-up/SignUp";
+import ExperienceComponent from "./components/center/ExperienceComponent"
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import ResourcesSinglePageComponent from "./components/center/ResourcesSinglePageComponent.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import MyProfile from "./components/global/MyProfile"
+import CustomNavbar from "./components/global/CustomNavbar.jsx"
+import { useSelector, useDispatch } from "react-redux"
+import { showUserSearchAction } from "./redux/actions"
+import MessagingPopup from "./components/global/MessagingPopup"
+import Feed from "./components/feed/Feed"
+import SearchDisplay from "./components/global/SearchDisplay.jsx"
+import OtherUser from "./components/global/OtherUser"
+import LoginPage from "./components/login/LoginPage"
+import Network from "./components/network/Network"
+import SignUp from "./components/sign-up/SignUp"
+import MyConnections from "./components/network/MyConnections"
 
 function App() {
-  let showSearchResults = useSelector((state) => state.showUsers.show);
-  const dispatch = useDispatch();
+  let showSearchResults = useSelector((state) => state.showUsers.show)
+  const dispatch = useDispatch()
   return (
     <BrowserRouter>
       <div className="App">
@@ -32,10 +33,8 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/me" element={<MyProfile />} />
           <Route path="/network" element={<Network />} />
-          <Route
-            path="/details/resources"
-            element={<ResourcesSinglePageComponent />}
-          />
+          <Route path="/mynetwork" element={<MyConnections />} />
+          <Route path="/details/resources" element={<ResourcesSinglePageComponent />} />
           <Route path="/editexperiences" element={<ExperienceComponent />} />
           <Route path="/home" element={<Feed />} />
           <Route path="/profile/:id" element={<OtherUser />} />
@@ -43,7 +42,7 @@ function App() {
       </div>
       <MessagingPopup />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/network/Network.jsx
+++ src/components/network/Network.jsx
@@ -1,58 +1,51 @@
-import { Container, Button, Form, Row, Col, Spinner } from "react-bootstrap";
-import * as Icon from "react-bootstrap-icons";
-import React from "react";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { getUsersAction } from "../../redux/actions";
-import FeedFooter from "../feed/FeedFooter";
-import "./styles.css";
-import Single_connection from "./Single_connection";
+import { Container, Button, Form, Row, Col, Spinner } from "react-bootstrap"
+import * as Icon from "react-bootstrap-icons"
+import React from "react"
+import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { getUsersAction } from "../../redux/actions"
+import FeedFooter from "../feed/FeedFooter"
+import "./styles.css"
+import Single_connection from "./Single_connection"
 const Network = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
   const fetchAllUsers = async () => {
     try {
-      const response = await fetch(
-        "https://linkedin-backend-production.up.railway.app/users/"
-      );
+      const response = await fetch("https://linkedin-backend-production.up.railway.app/users/")
       if (response.ok) {
-        const users = await response.json();
-        console.log("Users are here", users);
-        setUsers(users);
-        setLoading(false);
+        const users = await response.json()
+        console.log("Users are here", users)
+        setUsers(users)
+        setLoading(false)
       } else {
-        console.log("Error fetching data");
-        setLoading(false);
+        console.log("Error fetching data")
+        setLoading(false)
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   React.useEffect(() => {
-    fetchAllUsers().then(setLoading(true));
-  }, []);
-  const shuffledArray = users.sort((a, b) => 0.5 - Math.random());
+    fetchAllUsers().then(setLoading(true))
+  }, [])
+  const shuffledArray = users.sort((a, b) => 0.5 - Math.random())
   if (loading) {
     return (
       <div>
-        <div
-          className="d-flex justify-content-center align-items-center"
-          id="spinner"
-        >
+        <div className="d-flex justify-content-center align-items-center" id="spinner">
           <Spinner animation="border" className=" text-primary" />
         </div>
       </div>
-    );
+    )
   } else {
     return (
       <div style={{ color: "#5E5E5E" }}>
         <Container className="mt-4">
           <div className="d-flex ">
-            <div
-              className="bg-light mr-4 border"
-              style={{ borderRadius: "10px", width: "30%", height: "100%" }}
-            >
+            <div className="bg-light mr-4 border" style={{ borderRadius: "10px", width: "30%", height: "100%" }}>
               <div className=" mb-2">
                 <div className="pt-3 pl-3">Manage my network</div>
                 <div id="network-item" className=" mt-2">
@@ -60,7 +53,9 @@ const Network = () => {
                     <div className="mr-2">
                       <Icon.PeopleFill size={25} />
                     </div>
-                    <div>Connections</div>
+                    <Link to="/mynetwork">
+                      <div>Connections</div>
+                    </Link>
                   </div>
                   <span>65</span>
                 </div>
@@ -112,7 +107,7 @@ const Network = () => {
                 className="p-2"
                 style={{
                   borderBottom: "1px solid #a0a0a0",
-                  borderTop: "1px solid #a0a0a0",
+                  borderTop: "1px solid #a0a0a0"
                 }}
               >
                 <img
@@ -120,7 +115,7 @@ const Network = () => {
                   style={{
                     width: "100%",
 
-                    objectFit: "cover",
+                    objectFit: "cover"
                   }}
                 />
               </div>
@@ -128,21 +123,12 @@ const Network = () => {
                 <h5 className="text-dark">Add personal contacts</h5>
 
                 <p>
-                  We’ll periodically import and store your contacts to help you
-                  and others connect. You choose who to connect to and who to
-                  invite.
+                  We’ll periodically import and store your contacts to help you and others connect. You choose who to
+                  connect to and who to invite.
                 </p>
                 <div className="pr-2 pl-2">
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter username"
-                    className=""
-                  />
-                  <Button
-                    variant="outline-primary"
-                    className="pt-1 pb-1 mt-2"
-                    style={{ borderRadius: "25px" }}
-                  >
+                  <Form.Control type="email" placeholder="Enter username" className="" />
+                  <Button variant="outline-primary" className="pt-1 pb-1 mt-2" style={{ borderRadius: "25px" }}>
                     Continue
                   </Button>
                 </div>
@@ -157,10 +143,7 @@ const Network = () => {
                 <h6 className="m-0">No pending invitations</h6>
                 <h6 className="m-0">Manage</h6>
               </div>
-              <div
-                className="bg-light border p-3"
-                style={{ height: "200vh", borderRadius: "10px" }}
-              >
+              <div className="bg-light border p-3" style={{ height: "200vh", borderRadius: "10px" }}>
                 <div className="d-flex justify-content-between">
                   <h6> People who follow Shakira also follow </h6>
                   <h6> See all </h6>
@@ -169,7 +152,7 @@ const Network = () => {
                 <div>
                   <Row className="g-0">
                     {shuffledArray.map((connection, index) => {
-                      return <Single_connection {...connection} />;
+                      return <Single_connection {...connection} />
                     })}
                   </Row>
                 </div>
@@ -178,7 +161,7 @@ const Network = () => {
           </div>
         </Container>
       </div>
-    );
+    )
   }
-};
-export default Network;
+}
+export default Network
